611faa8b56934193c771ce1491c6a4205f0c9c62
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2024-05-02 13:29:30 -0500

    Update stream-pump-client.js

--- stream-pump-client.js
+++ stream-pump-client.js
@@ -1,18 +1,19 @@
-document.body='';
+document.body.textContent='';
 
 globalThis.streamQueue=[];
 
 setInterval(function(){
+  let output='';
   while(streamQueue.length){
-   output = streamQueue.shift(); 
-  }
-  if(output.lengh){
-    document.body.textContent = document.body.textContent+output;
+   output += streamQueue.shift(); 
+  }  
+  if(output.length){
+    document.body.textContent = document.body.textContent + output;
   }
 },100);
 
 void async function test(){
-    let testStream = (await zfetch(location.href)).zbody();
+    let testStream = (await zfetch(`${location.href}?${new Date().getTime()}`)).zbody();
     let testReader = testStream.zgetReader();
     while(true){
         const chunk = await zread(testReader);
